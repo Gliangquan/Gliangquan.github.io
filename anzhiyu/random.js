33d91bcd2d15f67ea6f27ce8d7f18a796756c881
liangquan <liangquan0000@outlook.com>
2024-08-31 15:22:42 +0800

    Site updated: 2024-08-31 15:22:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/31/idea配置模板语法/","2021/04/06/showMd/"];function toRandomPost(){
+var posts=["2021/04/06/showMd/","2024/08/31/idea配置模板语法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
