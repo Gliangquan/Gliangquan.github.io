4a50db793a5f1065f2e662c951ca5ebeda1420f0
liangquan <liangquan0000@outlook.com>
2024-08-31 12:19:09 +0800

    Site updated: 2024-08-31 12:19:09

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2021/04/06/testPage/","2021/04/06/testPage2/","2021/04/06/testPage3/","2021/04/06/testPage4/","2024/08/31/testPage5/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
