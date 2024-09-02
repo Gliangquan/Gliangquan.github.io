ca8916eed153a6cc6007a11e3ace3ff65bce338b
liangquan <liangquan0000@outlook.com>
2024-09-02 10:01:47 +0800

    Site updated: 2024-09-02 10:01:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/01/py基础/","2024/08/31/idea配置模板语法/","2021/04/06/showMd/","2024/08/31/深度学习环境配置/"];function toRandomPost(){
+var posts=["2024/08/31/idea配置模板语法/","2024/09/01/py基础/","2021/04/06/showMd/","2024/08/31/深度学习环境配置/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
